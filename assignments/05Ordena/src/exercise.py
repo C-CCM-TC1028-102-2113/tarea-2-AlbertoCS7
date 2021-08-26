c5955498eed3a61625574abbfbaee5c954dacb8e
AlbertoCS7 <89434313+AlbertoCS7@users.noreply.github.com>
2021-08-25 22:47:47 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,5 +1,20 @@
 def main():
     # Escribe el código adecuado para completar el programa
+    x = int(input("Ingresa el primer número: "))
+    y = int(input("Ingresa el segundo número: "))
+    z = int(input("Ingresa el tercer número: "))
+    if(x<y and y<z):
+        print( x, y, z)
+    else(y<x and x<z):
+        print( y, x, z)
+    else(z<x and x<y):
+        print(z, x, y)
+    else(z<y and y<x): 
+        print(z, y, x) 
+    else(x<z and z<y)
+        print(x, z, y)     
+    else(y<z and z<x):
+        print(y, z, x)
     pass
 
 
